1d62116e43cda7331e62e4439c396b77913f375e
ComeSarazin <sasacoco@orange.fr>
2023-09-28 15:01:55 +0200

    ajoutPhoto

--- bio.php
+++ bio.php
@@ -7,17 +7,16 @@ include "includes/php/_header.php";
 <main>
    
 <div class="container">
-  <div class="boxA">A</div>
+  <div class="photoProfile">
+    <img src="asset/images/wcsTronbiCarré-02.jpg">
+  </div>
   <div class="boxinfo">
     <p>Buchhold Etienne</p>
-    <p>32 ans</p>
-    <p>18/09/1991</p>
-    <p>Montreuil</p>
-    <p>etienne.buchhold@hotmail.com</p>
   </div>
   <div class="boxperso">C</div>
   <div class="boxD">D</div>
-  <div class="boxBio">E</div>
+  <div class="boxBio">Mon tout premier embarquement en tant que professionnel à bord d’un gros navire de pêche je partais pour pour 3 semaines. Entre le mal de mer et l’équipage atroce avec lequel j’étais j’ai failli, au bout de trois jours, quitter le bateau en sautant par dessus bord avec un gilet de sauvetage et une balise de détresse en espérant qu' un autre navire me trouve. Heureusement pour moi je ne l’ai pas fait car en mer du Nord il fait très froid. ça aurait été la plus grosse erreur de ma vie !
+</div>
 </div>
 
 
