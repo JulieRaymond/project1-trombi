08dbedc09ac3c6c7976c1086b18ed6409ddabe47
ComeSarazin <sasacoco@orange.fr>
2023-09-28 10:59:16 +0200

    créationGrid

---
+++ bio.php
@@ -0,0 +1,30 @@
+<?php
+$pageTitle = "Bio";
+$pageName = "bio";
+include "includes/php/_header.php";
+?>
+<link rel="stylesheet" href="bio.css" />
+<main>
+   
+<div class="container">
+  <div class="boxA">A</div>
+  <div class="boxinfo">
+    <p>Buchhold Etienne</p>
+    <p>32 ans</p>
+    <p>18/09/1991</p>
+    <p>Montreuil</p>
+    <p>etienne.buchhold@hotmail.com</p>
+  </div>
+  <div class="boxC">C</div>
+  <div class="boxD">D</div>
+  <div class="boxBio">E</div>
+</div>
+
+
+
+</main>
+
+
+<?php
+include "includes/php/_footer.php";
+?>
