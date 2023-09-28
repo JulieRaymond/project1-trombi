05f82dc23eb8812aff47ee1ab07f2f415e830bae
VIRGINIE  ROSA_ARSENEV <virginieralg@gmail.com>
2023-09-28 09:20:40 +0200

    first modif

---
+++ contact.php
@@ -0,0 +1,42 @@
+<?php
+$pageTitle = "Contact";
+$pageName = "contact";
+include "includes/php/_header.php";
+?>
+<main> 
+ 
+    <h1> Contactez-nous !</h1>
+    
+<p> Prêt à rencontrer nos super-devs ? <br>
+    
+    Remplisser le formulaire et laissez-nous coder !</p>
+
+          
+            <field> 
+            <form action="" method="get">
+              
+<div class="form-nom-email">
+
+        <div class="form-column"> 
+             <label>Nom et Prénom</label>
+              <input type="text>" name="nom&prénom" id="nom&prénom" size="40" placeholder="ex: WILDER Paul" autofocus required>
+        </div>
+
+        <div class="form-column"> 
+              <label>email</label>
+              <input type="email" placeholder="frg@gmail.com" required>
+        </div class>
+
+</div>
+
+              <label for="ameliorer">Message</label><br>
+              <textarea name="message" id="message" placeholder="Bonjour, ...." row="10" required> </textarea>
+              <input type="submit" value="envoyer">
+
+              </field>
+            
+            </form>
+        </main>
+        <?php
+include "includes/php/_footer.php";
+?>
