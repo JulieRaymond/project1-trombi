2712f2dfebe71a565a124c1bb346e6b0c917d568
Nicolas Leroux <hertford.fr@gmail.com>
2023-09-29 16:51:49 +0200

    bug bio

--- asset/scripts/bioClasse.php
+++ asset/scripts/bioClasse.php
@@ -1,25 +1,25 @@
 <?php
 class Biography
 {
-    protected string $lastName;
-    protected string $firstName;
-    protected string $astro;
-    protected string $linkedIn;
-    protected string $interest;
-    protected string $activity;
-    protected string $animal;
-    protected string $bio;
+    private string $lastName;
+    private string $firstName;
+    private string $astro;
+    private string $linkedIn;
+    private string $interest;
+    private string $activity;
+    private string $animal;
+    private string $bio;
 
     public function __construct($lastName, $firstName, $astro, $linkedIn, $interest, $activity, $animal, $bio)
     {
-        $this->$lastName = $lastName;
-        $this->$firstName = $firstName;
-        $this->$astro = $astro;
-        $this->$linkedIn = $linkedIn;
-        $this->$interest = $interest;
-        $this->$activity = $activity;
-        $this->$animal = $animal;
-        $this->$bio = $bio;
+        $this->lastName = $lastName;
+        $this->firstName = $firstName;
+        $this->astro = $astro;
+        $this->linkedIn = $linkedIn;
+        $this->interest = $interest;
+        $this->activity = $activity;
+        $this->animal = $animal;
+        $this->bio = $bio;
     }
 
     public function getLastName()
--- bio.php
+++ bio.php
@@ -1,25 +1,25 @@
 <?php
 $pageTitle = "Bio";
 $pageName = "bio";
-include "includes/php/_navbar.php";
+include "includes/php/_head.php";
 ?>
-<link rel="stylesheet" href="bio.css" />
+
 <main>
-   
-<div class="container">
-  <div class="photoProfile">
-    <img src="asset/images/wcsTronbiCarré-02.jpg">
+
+  <div class="container">
+    <div class="photoProfile">
+      <img src="asset/images/wcsTronbiCarré-02.jpg">
+    </div>
+    <div class="boxinfo">
+      <p>Buchhold Etienne</p>
+    </div>
+    <div class="boxperso">C</div>
+    <div class="photoPerso">
+      <img src="asset/images/wcsTronbiCarré-02.jpg">
+    </div>
+    <div class="boxBio">Mon tout premier embarquement en tant que professionnel à bord d’un gros navire de pêche je partais pour pour 3 semaines. Entre le mal de mer et l’équipage atroce avec lequel j’étais j’ai failli, au bout de trois jours, quitter le bateau en sautant par dessus bord avec un gilet de sauvetage et une balise de détresse en espérant qu' un autre navire me trouve. Heureusement pour moi je ne l’ai pas fait car en mer du Nord il fait très froid. ça aurait été la plus grosse erreur de ma vie !
+    </div>
   </div>
-  <div class="boxinfo">
-    <p>Buchhold Etienne</p>
-  </div>
-  <div class="boxperso">C</div>
-  <div class="photoPerso">
-  <img src="asset/images/wcsTronbiCarré-02.jpg">
-  </div>
-  <div class="boxBio">Mon tout premier embarquement en tant que professionnel à bord d’un gros navire de pêche je partais pour pour 3 semaines. Entre le mal de mer et l’équipage atroce avec lequel j’étais j’ai failli, au bout de trois jours, quitter le bateau en sautant par dessus bord avec un gilet de sauvetage et une balise de détresse en espérant qu' un autre navire me trouve. Heureusement pour moi je ne l’ai pas fait car en mer du Nord il fait très froid. ça aurait été la plus grosse erreur de ma vie !
-</div>
-</div>
 
 
 
