70ae57de48ddf1b3d5757acb5b34df04ac627253
VIRGINIE  ROSA_ARSENEV <virginieralg@gmail.com>
2023-09-30 18:02:52 +0200

    adding files footer.php and pagecontact.php pagecontact.css to replace contact files

---
+++ footer.php
@@ -0,0 +1,16 @@
+
+
+<footer class="test">
+        
+       <div class="footer" >
+                <div class="box0"> </div>
+        <div class="box1"> crédits </div>
+        
+        <div class= "box2"> 
+        <img src="asset/logos/pi-symbol.svg" alt="image du Signe Pi algèbre">
+        </div>
+       
+    
+    </div>
+
+    </footer>
---
+++ pagecontact.php
@@ -0,0 +1,45 @@
+<?php
+$pageTitle = "Contact";
+$pageName = "contact";
+
+include "includes/php/_head.php";
+
+
+?>
+<main> 
+ 
+    <h1>Contactez-nous !</h1>
+    
+<p> Prêt à rencontrer nos super-devs ? <br>
+    
+    Remplisser le formulaire et laissez-nous coder !</p>
+
+          
+            <field> 
+            <form action="" method="get">
+              
+                  
+             <label>Nom et Prénom</label>
+   
+              <input type="text>" name="nom&prénom" id="nom&prénom" size="40" placeholder="ex: WILDER Paul" autofocus required>
+
+
+              <label>email</label>
+
+              <input type="email" placeholder="frg@gmail.com" required>
+       
+
+              <label for="ameliorer">Message</label>
+
+              <textarea name="message" id="message" placeholder="Bonjour, ...." row="10" required> </textarea>
+              <br> <br>
+              <input type="submit" value="envoyer" class="cta">
+
+            
+            </form>
+            </field>
+
+        </main>
+        <?php
+include "includes/php/_footer.php";
+?>
