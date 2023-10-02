36017c48ec5d78fe48c1eabb971b4db371bb3280
Nicolas Leroux <hertford.fr@gmail.com>
2023-10-02 13:44:48 +0200

    petits bugs

--- includes/php/_footer.php
+++ includes/php/_footer.php
@@ -1,3 +1,14 @@
-</body>
+<footer class="test">
 
-</html>
+    <div class="footer">
+        <div class="box0"> </div>
+        <div class="box1"> crédits </div>
+
+        <div class="box2">
+            <img src="asset/logos/pi-symbol.svg" alt="image du Signe Pi algèbre">
+        </div>
+
+
+    </div>
+
+</footer>
