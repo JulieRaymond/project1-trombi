19ea2f6fb31eb3a37f22fca2691747c71f167d1d
Nicolas Leroux <hertford.fr@gmail.com>
2023-10-05 10:54:43 +0200

    modif bio

--- bio.php
+++ bio.php
@@ -1,4 +1,10 @@
 <?php
+include "asset/scripts/biowrite.php";
+if (!empty($_GET) && in_array($_GET['name'], array_keys($phpGroup))) {
+    $name = $phpGroup[$_GET['name']];
+} else {
+    $name = $phpGroup["sami"];
+}
 $pageTitle = "Bio";
 $pageName = "bio";
 include "includes/php/_head.php";
@@ -6,23 +12,29 @@ include "includes/php/_head.php";
 
 <main>
 
-  <div class="container">
-      <div class="photoProfile">
-          <img src="asset/images/wcsTronbiCarré-02.jpg">
-      </div>
-      <div class="boxinfo">
-          <p>Buchhold Etienne</p>
-      </div>
-      <div class="boxperso">
-           C
-      </div>
-      <div class="photoPerso">
-          <img src="asset/images/wcsTronbiCarré-02.jpg">
-      </div>
-      <div class="boxBio">
-          <p>Mon tout premier embarquement en tant que professionnel à bord d’un gros navire de pêche je partais pour pour 3 semaines. Entre le mal de mer et l’équipage atroce avec lequel j’étais j’ai failli, au bout de trois jours, quitter le bateau en sautant par dessus bord avec un gilet de sauvetage et une balise de détresse en espérant qu' un autre navire me trouve. Heureusement pour moi je ne l’ai pas fait car en mer du Nord il fait très froid. ça aurait été la plus grosse erreur de ma vie !</p>
-      </div>
-  </div>
+    <div class="container">
+        <div class="photoProfile">
+            <img src="<?= $name->getImage() ?>" <?= $name->getSrcAndSizes() ?> alt="Photo de <?= $name->getFirstName() ?>">
+        </div>
+        <div class=" boxinfo">
+            <h1>
+                <?= $name->getFirstName() ?> <?= $name->getLastName() ?>
+            </h1>
+            <a href="<?= $name->getLinkedIn() ?>"><img src="asset/logos/linkedin.svg" alt="logo linkedin"></a>
+        </div>
+        <div class="boxperso">
+            <p>Signe astrologique : <?= $name->getAstro() ?><br>
+                Centre(s) d'intérêt : <?= $name->getInterest() ?><br>
+                Activités : <?= $name->getActivity() ?><br>
+                Animal: <?= $name->getAnimal() ?></p>
+        </div>
+        <div class="photoPerso">
+            <img src="<?= $name->getImage() ?>" <?= $name->getSrcAndSizes() ?> alt="Photo de <?= $name->getFirstName() ?>">
+        </div>
+        <div class="boxBio">
+            <p><?= $name->getBio() ?></p>
+        </div>
+    </div>
 
 
 
