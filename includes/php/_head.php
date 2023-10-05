3a06b91ebd0e396977edd5613aa73ffe87213868
Nicolas Leroux <hertford.fr@gmail.com>
2023-10-05 20:18:56 +0200

    ajout margin sur credits.php pour width > 992px. Ne fonctionnait pas quand la règle s'appliquait sur le .contenair. J'ai rajouté une classe à cette div J'ai aussi ajouté la favicon dans _head.php

--- includes/php/_head.php
+++ includes/php/_head.php
@@ -15,14 +15,16 @@ if (isset($_GET["name"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle ?></title>
+    <link rel="icon" href="asset/logos/faviconclaire.png" />
     <link rel="stylesheet" href="../includes/css/generalStyle.css">
     <link rel="stylesheet" href="../includes/css/_navbar.css">
-    <link rel="stylesheet" href="../includes/css/_footer.css">
+
     <?php
     if (isset($pageName)) {
         echo '<link rel="stylesheet" href="../includes/css/' . $pageName . '.css">';
     }
     ?>
+    <link rel="stylesheet" href="../includes/css/_footer.css">
 </head>
 <?php include "includes/php/_navbar.php" ?>
 
