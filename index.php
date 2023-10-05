f7a3a90314cad736f4f4a1b22bab02eed5fae2ea
Julie <raymondjulie@proton.me>
2023-10-05 14:07:20 +0200

    div image index auto et js auto

--- index.php
+++ index.php
@@ -3,31 +3,30 @@
   $pageName = "index";
   include "includes/php/_head.php";
   include "asset/scripts/biowrite.php";
+  $counter = 0;
   ?>
-  <main>
-    <?php
 
-    //var_dump($phpGroup);
-    ?>
+  <main>
     <div id="container">
       <h1>Trombinoscope</h1>
-      <div class="field desktop"><a href="bio.php"><img src="/asset/images/crop/images1024/adeline1024.jpg" srcset="" alt="adeline" /></a></div>
-      <div class="field desktop"><a href=""><img src="/asset/images/crop/images1024/come1024.jpg" alt="come" /></a></div>
-      <div class="field desktop"><a href=""><img src="/asset/images/crop/images1024/etienne1024.jpg" alt="etienne" /></a></div>
-      <div class="field desktop"><a href=""><img src="/asset/images/crop/images1024/johann1024.jpg" alt="johann" /></a></div>
-      <div class="field desktop"><a href=""><img src="/asset/images/crop/images1024/jonathan1024.jpg" alt="jonathan" /></a></div>
-      <div class="field desktop"><a href=""><img src="/asset/images/crop/images1024/julie1024.jpg" alt="julie" /></a></div>
-      <div class="field desktop"><a href=""><img src="/asset/images/crop/images1024/marion1024.jpg" alt="marion" /></a></div>
-      <div class="field desktop"><a href=""><img src="/asset/images/crop/images1024/martha1024.jpg" alt="martha" /></a></div>
-      <div class="field desktop"><a href=""><img src="/asset/images/crop/images1024/melissa1024.jpg" alt="melissa" /></a></div>
-      <div class="field desktop"><a href=""><img src="/asset/images/crop/images1024/nicolas1024.jpg" alt="nicolas" /></a></div>
-      <div class="field mobil"><a href=""><img src="/asset/images/crop/images1024/sami1024.jpg" alt="sami" /></a></div>
-      <div class="field mobil"><a href=""><img src="/asset/images/crop/images1024/severine1024.jpg" alt="severine" /></a></div>
-      <div class="field mobil"><a href=""><img src="/asset/images/crop/images1024/victoria1024.jpg" alt="victoria" /></a></div>
-      <div class="field mobil"><a href=""><img src="/asset/images/crop/images1024/virginie1024.jpg" alt="virginie" /></a></div>
-      <div class="field mobil"><a href=""><img src="/asset/images/crop/images1024/julien1024.jpg" alt="julien" /></a></div>
+      <?php foreach ($phpGroup as $person) { ?>
+        <div class="field 
+        <?php if ($counter < 10) {
+          echo "desktop";
+        } else {
+          echo "mobil";
+        }
+        ?>">
+          <a href="<?php echo $person->getPage(); ?>">
+            <img src="<?php echo $person->getImage(); ?>" srcset="<?php echo $person->getSrcset(); ?>" sizes="<?php echo $person->getSizes(); ?>" alt="<?php echo $person->getFirstName(); ?>" />
+          </a>
+        </div>
+      <?php
+        $counter += 1;
+      } ?>
     </div>
   </main>
+
   <?php
   include "includes/php/_footer.php";
   ?>
