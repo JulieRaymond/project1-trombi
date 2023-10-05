96d8b10d5dd981c69de25d73e97f97d43dfb3b41
ComeSarazin <sasacoco@orange.fr>
2023-10-05 17:10:05 +0200

    nouvelle Nav et Modife container de l'index

--- includes/php/_navbar.php
+++ includes/php/_navbar.php
@@ -1,12 +1,39 @@
 <?php
 ?>
 <nav>
-    <a href="index.php"><img src="asset/logos/logo-php.svg" id="logo" /></a>
-    <label id="label_for_menu" for="toggle">☰</label>
-    <input type="checkbox" id="toggle">
-    <div class="main_pages">
-        <a href="aboutus.php">A propos de nous</a>
-        <a href="group.php">Groupe PHP</a>
-        <a href="pagecontact.php">contact</a>
+    <div class="desktopNav">
+        <div id="logoNavDesktop">
+            <a href="index.php"><img src="asset/logos/logo-php.svg" id="logo" /></a>
+        </div>
+        <div id="linksNavDesktop">
+            <a href="aboutus.php">A propos de nous</a>
+            <a href="group.php">Groupe PHP</a>
+            <a href="pagecontact.php">contact</a>
+        </div>
     </div>
+
+    <div class="mobileNav">
+        <div id="logoNavMobile">
+            <a href="index.php"><img src="asset/logos/logo-php.svg" id="logo" /></a>
+        </div>
+        <div id="linksNavMobile">
+            <button class="dropbtn">☰</button>
+            <div class="menu-content">
+                <a href="aboutus.php">A propos de nous</a>
+                <a href="group.php">Groupe PHP</a>
+                <a href="pagecontact.php">contact</a>
+            </div>
+         </div>
+    </div>
+    <!-- 
+
+    <a href="index.php"><img src="asset/logos/logo-php.svg" id="logo" /></a>
+        <label id="label_for_menu" for="toggle">☰</label>
+        <input type="checkbox" id="toggle">
+        <div class="main_pages">
+            <a href="aboutus.php">A propos de nous</a>
+            <a href="group.php">Groupe PHP</a>
+            <a href="pagecontact.php">contact</a>
+        </div>
+    -->
 </nav>
