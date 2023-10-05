2628025c70dfa6f09598a8880dc82ebce69356e7
Sami Ahamadi <sami.ahamadi@laposte.net>
2023-10-05 21:32:37 +0200

    push exemple William

---
+++ example.php
@@ -0,0 +1,104 @@
+<html>
+<head>
+    <style>
+        html body {
+            margin: 0;
+        }
+
+        #superContainer {
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+        }
+        nav {
+            min-height: 80px;
+            height: 80px;
+            background-color: red;
+        }
+
+        footer {
+            min-height: 30px;
+            height: 30px;
+            background-color: red;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div id="superContainer">
+        <nav>NAV</nav>
+        <main>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+
+            v            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+            <h1>MAIN</h1>
+
+        </main>
+        <footer>
+            footer
+        </footer>
+
+    </div>
+</body>
+</html>
